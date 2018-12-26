51ba6cd5096414f6bcb077f9fef1215762a5444c
Nick Law <nick@nicholaslaw.com.au>
2018-12-27 05:13:57 +1100

    Add some methods for identifying webhooks events

--- webhooks_test.go
+++ webhooks_test.go
@@ -2,6 +2,7 @@ package helix
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -118,6 +119,136 @@ func TestPostWebhookSubscriptions(t *testing.T) {
 			}
 
 			continue
+		}
+	}
+}
+
+func newMockWebhookRequest(header string) *http.Request {
+	return &http.Request{Header: map[string][]string{"Link": []string{header}}}
+}
+
+func TestGetWebhookTopicFromRequest(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		req   *http.Request
+		topic WebhookTopic
+	}{
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&from_id=1234&to_id=5678>; rel=\"self\""),
+			UserFollowsTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&from_id=1234>; rel=\"self\""),
+			UserFollowsTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&to_id=1234>; rel=\"self\""),
+			UserFollowsTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/streams?user_id=1234>; rel=\"self\""),
+			StreamChangedTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users?id=1234>; rel=\"self\""),
+			UserChangedTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/analytics?game_id=1a2b3c4d>; rel=\"self\""),
+			GameAnalyticsTopic,
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/analytics?extension_id=1a2b3c4d>; rel=\"self\""),
+			ExtensionAnalyticsTopic,
+		},
+		{
+			newMockWebhookRequest("bad header value"),
+			-1,
+		},
+		{
+			newMockWebhookRequest(""),
+			-1,
+		},
+		{
+			&http.Request{},
+			-1,
+		},
+	}
+
+	for _, testCase := range testCases {
+		topic := GetWebhookTopicFromRequest(testCase.req)
+		if topic != testCase.topic {
+			t.Errorf("expected webhook topic to be \"%d\", got \"%d\"", testCase.topic, topic)
+		}
+	}
+}
+
+func TestGetWebhookTopicValuesFromRequest(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		req    *http.Request
+		topic  WebhookTopic
+		values map[string]string
+	}{
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&from_id=1234&to_id=5678>; rel=\"self\""),
+			UserFollowsTopic,
+			map[string]string{"from_id": "1234", "to_id": "5678"},
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&from_id=1234>; rel=\"self\""),
+			UserFollowsTopic,
+			map[string]string{"from_id": "1234", "to_id": ""},
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users/follows?first=1&to_id=5678>; rel=\"self\""),
+			UserFollowsTopic,
+			map[string]string{"from_id": "", "to_id": "5678"},
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/streams?user_id=1234>; rel=\"self\""),
+			StreamChangedTopic,
+			map[string]string{"user_id": "1234"},
+		},
+
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/users?id=1234>; rel=\"self\""),
+			UserChangedTopic,
+			map[string]string{"id": "1234"},
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/analytics?game_id=1a2b3c4d>; rel=\"self\""),
+			GameAnalyticsTopic,
+			map[string]string{"game_id": "1a2b3c4d"},
+		},
+		{
+			newMockWebhookRequest("<https://api.twitch.tv/helix/webhooks/hub>; rel=\"hub\", <https://api.twitch.tv/helix/analytics?extension_id=1a2b3c4d>; rel=\"self\""),
+			ExtensionAnalyticsTopic,
+			map[string]string{"extension_id": "1a2b3c4d"},
+		},
+		{
+			newMockWebhookRequest("bad header value"),
+			-1,
+			make(map[string]string),
+		},
+		{
+			newMockWebhookRequest(""),
+			-1,
+			make(map[string]string),
+		},
+		{
+			&http.Request{},
+			-1,
+			make(map[string]string),
+		},
+	}
+
+	for _, testCase := range testCases {
+		values := GetWebhookTopicValuesFromRequest(testCase.req, testCase.topic)
+		if !reflect.DeepEqual(values, testCase.values) {
+			t.Errorf("expected webhook values to be \"%v\", got \"%v\"", testCase.values, values)
 		}
 	}
 }
