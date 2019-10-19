c45a65f60c6626aadffb6bb14f38ea8843d3ca17
Brian M <bmichalski@gmail.com>
2019-10-19 14:29:14 -0400

    Replace CommunityIDs with TagIDs.

--- streams.go
+++ streams.go
@@ -8,7 +8,7 @@ type Stream struct {
 	UserID       string    `json:"user_id"`
 	UserName     string    `json:"user_name"`
 	GameID       string    `json:"game_id"`
-	CommunityIDs []string  `json:"community_ids"`
+	TagIDs       []string  `json:"tag_ids"`
 	Type         string    `json:"type"`
 	Title        string    `json:"title"`
 	ViewerCount  int       `json:"viewer_count"`
@@ -31,15 +31,14 @@ type StreamsResponse struct {
 
 // StreamsParams ...
 type StreamsParams struct {
-	After        string   `query:"after"`
-	Before       string   `query:"before"`
-	CommunityIDs []string `query:"community_id"`
-	First        int      `query:"first,20"`   // Limit 100
-	GameIDs      []string `query:"game_id"`    // Limit 100
-	Language     []string `query:"language"`   // Limit 100
-	Type         string   `query:"type,all"`   // "all" (default), "live" and "vodcast"
-	UserIDs      []string `query:"user_id"`    // limit 100
-	UserLogins   []string `query:"user_login"` // limit 100
+	After      string   `query:"after"`
+	Before     string   `query:"before"`
+	First      int      `query:"first,20"`   // Limit 100
+	GameIDs    []string `query:"game_id"`    // Limit 100
+	Language   []string `query:"language"`   // Limit 100
+	Type       string   `query:"type,all"`   // "all" (default), "live" and "vodcast"
+	UserIDs    []string `query:"user_id"`    // limit 100
+	UserLogins []string `query:"user_login"` // limit 100
 }
 
 // GetStreams ...
