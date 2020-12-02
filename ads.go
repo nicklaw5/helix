cb907eadf1c894879a87c50c9e67708a998d55b8
intel352 <jon@langevin.me>
2020-12-02 01:37:34 -0500

    Adding Start Commercial endpoint support

---
+++ ads.go
@@ -0,0 +1,51 @@
+package helix
+
+type AdLengthEnum int
+
+const (
+	_ AdLengthEnum = iota * 30
+	AdLen30
+	AdLen60
+	AdLen90
+	AdLen120
+	AdLen150
+	AdLen180
+)
+
+// StartCommercialParams ...
+type StartCommercialParams struct {
+	BroadcasterID string       `query:"broadcaster_id"`
+	Length        AdLengthEnum `query:"length"`
+}
+
+type AdDetails struct {
+	Length     AdLengthEnum `json:"length"`
+	Message    string       `json:"message"`
+	RetryAfter int          `json:"retry_after"`
+}
+
+type ManyAdDetails struct {
+	AdDetails []AdDetails `json:"data"`
+}
+
+// StartCommercialResponse
+type StartCommercialResponse struct {
+	ResponseCommon
+	Data ManyAdDetails
+}
+
+// StartCommercial starts a commercial on a specified channel
+// OAuth Token required
+// Requires channel:edit:commercial scope
+func (c *Client) StartCommercial(params *StartCommercialParams) (*StartCommercialResponse, error) {
+	resp, err := c.post("/channels/commercial", &ManyAdDetails{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	commercials := &StartCommercialResponse{}
+	resp.HydrateResponseCommon(&commercials.ResponseCommon)
+	commercials.Data.AdDetails = resp.Data.(*ManyAdDetails).AdDetails
+
+	return commercials, nil
+}
---
+++ ads_test.go
@@ -0,0 +1,89 @@
+package helix
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestClient_StartCommercial(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		statusCode     int
+		options        *Options
+		count          int
+		broadcasterId  string
+		adLength       AdLengthEnum
+		respBody       string
+		expectedErrMsg string
+	}{
+		// TODO: expand with other test cases
+
+		// Failure - broadcaster not live
+		{
+			http.StatusBadRequest,
+			&Options{ClientID: "my-client-id"},
+			1,
+			"89754791",
+			AdLen30,
+			`{"error":"Bad Request","status":400,"message":"the channel 'codingsloth' is not currently live and needs to be in order to start commercials."}`,
+			"the channel 'codingsloth' is not currently live and needs to be in order to start commercials.",
+		},
+		// success
+		{
+			http.StatusOK,
+			&Options{ClientID: "my-client-id"},
+			1,
+			"41245072",
+			AdLen60,
+			`{"data":[{"length":60,"message":"","retry_after":480}]}`,
+			"",
+		},
+	}
+
+	for _, testCase := range testCases {
+		c := newMockClient(testCase.options, newMockHandler(testCase.statusCode, testCase.respBody, nil))
+
+		params := &StartCommercialParams{
+			BroadcasterID: testCase.broadcasterId,
+			Length:        testCase.adLength,
+		}
+
+		resp, err := c.StartCommercial(params)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if resp.StatusCode != testCase.statusCode {
+			t.Errorf("expected status code to be \"%d\", got \"%d\"", testCase.statusCode, resp.StatusCode)
+		}
+
+		// Test error cases
+		if resp.StatusCode != http.StatusOK {
+			if resp.ErrorStatus != testCase.statusCode {
+				t.Errorf("expected error status to be \"%d\", got \"%d\"", testCase.statusCode, resp.ErrorStatus)
+			}
+
+			if resp.ErrorMessage != testCase.expectedErrMsg {
+				t.Errorf("expected error message to be \"%s\", got \"%s\"", testCase.expectedErrMsg, resp.ErrorMessage)
+			}
+
+			continue
+		}
+
+		// Test success cases
+		if len(resp.Data.AdDetails) != testCase.count {
+			t.Errorf("expected number of results to be \"%d\", got \"%d\"", testCase.count, len(resp.Data.AdDetails))
+		}
+
+		commercialDetails := resp.Data.AdDetails[0]
+		if commercialDetails.Length != testCase.adLength {
+			t.Error("expected ad length \"#{testCase.adLength}\", got \"#{commercialDetails.Length}\"")
+		}
+
+		if commercialDetails.Message != "" {
+			t.Error("expected an empty error message, got \"#{commercialDetails.Message}\"")
+		}
+
+	}
+}
