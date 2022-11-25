23f8fd8d124154df8292993e15e2fb072a38f03d
Jack McGuire <jackmcguire35@gmail.com>
2022-11-25 09:44:36 +0000

    feat: charity updates
    - add new charity eventsubs
    - add new charity donations endpoint
    - add unit test for charity donations

--- charity.go
+++ charity.go
@@ -1,5 +1,29 @@
 package helix
 
+type CharityDonationData struct {
+	CampaignID       string                `json:"campaign_id"`
+	BroadcasterID    string                `json:"user_id"`
+	BroadcasterName  string                `json:"user_name"`
+	BroadcasterLogin string                `json:"user_login"`
+	Amount           CharityCampaignAmount `json:"amount"`
+}
+
+type ManyCharityDonations struct {
+	Donations  []CharityDonationData `json:"data"`
+	Pagination Pagination            `json:"pagination"`
+}
+
+type CharityDonationParams struct {
+	BroadcasterID string `query:"broadcaster_id"`
+	After         string `query:"after"`
+	First         int    `query:"first,20"` // Limit 100
+}
+
+type CharityDonationsResponse struct {
+	ResponseCommon
+	Data ManyCharityDonations
+}
+
 type CharityCampaignAmount struct {
 	Value         int64  `json:"amount"`
 	DecimalPlaces int64  `json:"decimal_places"`
@@ -46,6 +70,21 @@ func (c *Client) GetCharityCampaigns(params *CharityCampaignsParams) (*CharityCa
 	resp.HydrateResponseCommon(&events.ResponseCommon)
 	events.Data.Campaigns = resp.Data.(*ManyCharityCampaigns).Campaigns
 	events.Data.Pagination = resp.Data.(*ManyCharityCampaigns).Pagination
+
+	return events, nil
+}
+
+// Required scope: channel:read:charity
+func (c *Client) GetCharityDonations(params *CharityDonationParams) (*CharityDonationsResponse, error) {
+	resp, err := c.get("/charity/donations", &ManyCharityDonations{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	events := &CharityDonationsResponse{}
+	resp.HydrateResponseCommon(&events.ResponseCommon)
+	events.Data.Donations = resp.Data.(*ManyCharityDonations).Donations
+	events.Data.Pagination = resp.Data.(*ManyCharityDonations).Pagination
 
 	return events, nil
 }
