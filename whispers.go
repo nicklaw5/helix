621f8efd4738f948531029de2e85b72c37cdacb3
Jack McGuire <jackmcguire35@gmail.com>
2022-12-07 09:29:13 +0000

    fix: comment

--- whispers.go
+++ whispers.go
@@ -17,9 +17,9 @@ type SendUserWhisperResponse struct {
 // You may whisper to a maximum of 40 unique recipients per day. Within the per day limit.
 // You may whisper a maximum of 3 whispers per second and a maximum of 100 whispers per minute.
 // Message character limit:
-//  - Max 500 chars to new recipient
-//  - Max 10,000 if recurring recipient,
-//  - > 10,000 chars are truncated.
+//   - Max 500 chars to new recipient
+//   - Max 10,000 if recurring recipient,
+//   - > 10,000 chars are truncated.
 func (c *Client) SendUserWhisper(params *SendUserWhisperParams) (*SendUserWhisperResponse, error) {
 	resp, err := c.postAsJSON("/whispers", nil, params)
 	if err != nil {
