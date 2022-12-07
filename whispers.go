83ae92efa0cd376a2302e897393f78d994761be0
Jack McGuire <jackmcguire35@gmail.com>
2022-12-07 09:11:33 +0000

    fix: comments

--- whispers.go
+++ whispers.go
@@ -11,15 +11,15 @@ type SendUserWhisperResponse struct {
 }
 
 // SendUserWhisper
-// requires user access token with user:manage:whispers scope.
+// Requires user access token with user:manage:whispers scope.
 // The user sending the whisper must have a verified phone number.
 // The API may silently drop whispers that it suspects of violating Twitch policies 204 still returned.
 // You may whisper to a maximum of 40 unique recipients per day. Within the per day limit.
-// you may whisper a maximum of 3 whispers per second and a maximum of 100 whispers per minute.
-// messages character limit:
-//   - 500 chars to new recipient
-//   - 10,000 if recurring recipient,
-//   - > 10,000 chars are truancated.
+// You may whisper a maximum of 3 whispers per second and a maximum of 100 whispers per minute.
+// Message character limit:
+//  - Max 500 chars to new recipient
+//  - Max 10,000 if recurring recipient,
+//  - > 10,000 chars are truncated.
 func (c *Client) SendUserWhisper(params *SendUserWhisperParams) (*SendUserWhisperResponse, error) {
 	resp, err := c.postAsJSON("/whispers", nil, params)
 	if err != nil {
