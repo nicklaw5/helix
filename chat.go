0e4c80c9b0b578d76d3651805c1631dfc4b07530
Jack McGuire <jackmcguire35@gmail.com>
2023-01-12 10:53:15 +0000

    fix: duplicate chat send announcements
    - remove duplicate send chat announcements

--- chat.go
+++ chat.go
@@ -194,21 +194,7 @@ func (c *Client) GetEmoteSets(params *GetEmoteSetsParams) (*GetEmoteSetsResponse
 // SendChatAnnouncement sends an announcement to the broadcaster’s chat room.
 // Required scope: moderator:manage:announcements
 func (c *Client) SendChatAnnouncement(params *SendChatAnnouncementParams) (*SendChatAnnouncementResponse, error) {
-    resp, err := c.post("/chat/announcements", nil, params)
-    if err != nil {
-        return nil, err
-    }
-
-	chatResp := &SendChatAnnouncementResponse{}
-	resp.HydrateResponseCommon(&chatResp.ResponseCommon)
-
-	return chatResp, nil
-}
-
-// SendChatAnnouncement sends an announcement to the broadcaster’s chat room.
-// Required scope: moderator:manage:announcements
-func (c *Client) SendChatAnnouncement(params *SendChatAnnouncementParams) (*SendChatAnnouncementResponse, error) {
-	resp, err := c.postAsJSON("/chat/announcements", nil, params)
+	resp, err := c.post("/chat/announcements", nil, params)
 	if err != nil {
 		return nil, err
 	}
