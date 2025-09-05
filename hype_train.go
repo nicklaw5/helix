37a64f4b5875a9d3b106ce1b6692109056bdd5ca
jackmc <jackmcguire35@gmail.com>
2025-09-05 14:24:52 +0100

    feat: hypetrain get status

--- hype_train.go
+++ hype_train.go
@@ -6,6 +6,62 @@ type HypeTrainContribuition struct {
 	User  string `json:"user"`
 }
 
+// HypeTrainStatusContribution contains information about a contribution to a Hype Train
+type HypeTrainStatusContribution struct {
+	UserID    string `json:"user_id"`
+	UserLogin string `json:"user_login"`
+	UserName  string `json:"user_name"`
+	Type      string `json:"type"`
+	Total     int64  `json:"total"`
+}
+
+type SharedTrainParticipant struct {
+	BroadcasterUserID    string `json:"broadcaster_user_id"`
+	BroadcasterUserLogin string `json:"broadcaster_user_login"`
+	BroadcasterUserName  string `json:"broadcaster_user_name"`
+}
+
+type CurrentHypeTrainStatus struct {
+	ID                      string                        `json:"id"`
+	BroadcasterUserID       string                        `json:"broadcaster_user_id"`
+	BroadcasterUserLogin    string                        `json:"broadcaster_user_login"`
+	BroadcasterUserName     string                        `json:"broadcaster_user_name"`
+	Level                   int64                         `json:"level"`
+	Total                   int64                         `json:"total"`
+	Progress                int64                         `json:"progress"`
+	Goal                    int64                         `json:"goal"`
+	TopContributions        []HypeTrainStatusContribution `json:"top_contributions"`
+	SharedTrainParticipants []SharedTrainParticipant      `json:"shared_train_participants"`
+	StartedAt               Time                          `json:"started_at"`
+	ExpiresAt               Time                          `json:"expires_at"`
+	Type                    string                        `json:"type"`
+}
+
+type AllTimeHighHypeTrainStatus struct {
+	Level      int64 `json:"level"`
+	Total      int64 `json:"total"`
+	AchievedAt Time  `json:"achieved_at"`
+}
+
+type HypeTrainStatus struct {
+	Current            *CurrentHypeTrainStatus     `json:"current"`
+	AllTimeHigh        *AllTimeHighHypeTrainStatus `json:"all_time_high"`
+	SharedAllTimeHigh  *AllTimeHighHypeTrainStatus `json:"shared_all_time_high"`
+}
+
+type ManyHypeTrainStatuses struct {
+	Statuses []HypeTrainStatus `json:"data"`
+}
+
+type HypeTrainStatusResponse struct {
+	ResponseCommon
+	Data ManyHypeTrainStatuses
+}
+
+type HypeTrainStatusParams struct {
+	BroadcasterID string `query:"broadcaster_id"`
+}
+
 type HypeTrainEvent struct {
 	ID             string             `json:"id"`
 	EventType      string             `json:"event_type"`
@@ -44,6 +100,23 @@ type HypeTrainEventsParams struct {
 	ID            string `query:"id"`
 }
 
+// GetHypeTrainStatus gets the Hype Train status for the specified broadcaster.
+// Required scope: channel:read:hype_train
+func (c *Client) GetHypeTrainStatus(params *HypeTrainStatusParams) (*HypeTrainStatusResponse, error) {
+	resp, err := c.get("/hypetrain/status", &ManyHypeTrainStatuses{}, params)
+	if err != nil {
+		return nil, err
+	}
+
+	status := &HypeTrainStatusResponse{}
+	resp.HydrateResponseCommon(&status.ResponseCommon)
+	status.Data.Statuses = resp.Data.(*ManyHypeTrainStatuses).Statuses
+
+	return status, nil
+}
+
+// GetHypeTrainEvents gets Hype Train events for a broadcaster.
+// Deprecated: Use GetHypeTrainStatus instead.
 // Required scope: channel:read:hype_train
 func (c *Client) GetHypeTrainEvents(params *HypeTrainEventsParams) (*HypeTrainEventsResponse, error) {
 	resp, err := c.get("/hypetrain/events", &ManyHypeTrainEvents{}, params)
